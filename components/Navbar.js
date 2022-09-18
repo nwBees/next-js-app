6675dd5f3632c54536563f327d08af0aaab08b72
tdanielles <trisha.d.sia@gmail.com>
2022-09-18 04:40:24 -0400

    more merges

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <Flex w="100%" position="fixed" top="0" backgroundColor="#fdf8f4" height="70px" flexDirection="row">
-        <Text fontSize={30} color="#5B8ADC" pl="20px" pr="1000px" alignSelf="center">Cofyde</Text>
+        <Text fontSize={30} color="#5B8ADC" pl="20px" pr="1140px" alignSelf="center">Cofyde</Text>
         <Button _hover={{color: "white", backgroundColor: "#5B8ADC"}} alignSelf="center" border="2px #5B8ADC solid" backgroundColor="#fdf8f4" color="#5B8ADC">Talk to someone</Button>
     </Flex>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import { Flex, Img, Text } from '@chakra-ui/react';
+import { Button, Flex, Icon, Img, Text } from '@chakra-ui/react';
 import Head from 'next/head'
 import Link from 'next/link';
 import Navbar from '../components/Navbar';
+import {FaAngleDoubleDown} from 'react-icons/fa'
 
 export default function Home() {
   return (
@@ -12,10 +13,14 @@ export default function Home() {
       </Head>
       <Flex position="absolute" zIndex={1000} flexDirection="column" alignItems="center">
         <Navbar/>
-        <Text zIndex={0} position="absolute" pt="220px" fontSize={45} color="#6da9d6">Cofyde</Text>
+        <Text zIndex={0} fontWeight="bold" position="absolute" pt="220px" fontSize={45} color="#6da9d6">Cofyde</Text>
         <Text zIndex={0} position="absolute" pt="315px" fontSize={20} color="#6da9d6">Where help doesn't wait.</Text>
+        <Text zIndex={0} position="absolute" pt="615px" fontSize={20} color="white"> Learn more <Icon as={FaAngleDoubleDown} top="2px" position="relative" boxSize={4}/> </Text>
+        <Text zIndex={0} fontWeight="bold" position="absolute" pt="2180px" fontSize={40} color="#6da9d6">Get Started</Text>
+        <Button height="60px" fontSize={14} _hover={{color: "white", backgroundColor: "#5B8ADC"}} position="absolute" pt="20px" pb="20px" mt="2280px" border="2px #6da9d6 solid" backgroundColor="#00000000" color="#6da9d6">Talk to someone</Button>
+        <Text zIndex={0} position="absolute" pt="2380px" fontSize={20} color="#6da9d6"> Admin? Enter <Link textDecoration="underline" href="/admin">here</Link> </Text>
         <Img src="/hero.png"/>
-        <Img src="/about.png"/>
+        <Img id='about' src="/about.png"/>
         <Img src="/start.png"/>
       </Flex>
     </div>
