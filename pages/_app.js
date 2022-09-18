c9db9b6ba6b2a8a78d8c69c44e8fa7a65451c983
tdanielles <trisha.d.sia@gmail.com>
2022-09-18 04:12:36 -0400

    more updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
-// `pages/_app.js`
-import '../styles/global.css';
+import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <ChakraProvider>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    )
 }
+
+// pages/_app.js
+// import { ChakraProvider } from '@chakra-ui/react'
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <ChakraProvider>
+//       <Component {...pageProps} />
+//     </ChakraProvider>
+//   )
+// }
+
+// export default MyApp
